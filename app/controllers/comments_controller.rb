12d15fe4640bff9f790c1d7b99723e11a307a3ea
syed.burhanuddin <syed@green-apex.com>
2023-06-10 19:21:29 +0530

    Fixed deletes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,22 @@
 class CommentsController < ApplicationController
 
+	before_action :find_post, only: [:create, :destroy]
+
 	def create
-		@post = Post.find(params[:post_id])
 	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
 		redirect_to post_path(@post)	
 	end
 
 	def destroy
-		@post = Post.find(params[:post_id])
+		
 		@comment = @post.comments.find(params[:id])
 		@comment.destroy
 		redirect_to post_path(@post)
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:post_id])
 	end
 end
